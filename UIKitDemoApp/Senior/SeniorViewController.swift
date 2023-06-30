f00dc8e1e095fe428ea891671303d66678145d09
Robin.J.Y.ZHONG <15914468806@139.com>
2023-06-30 17:59:05 +0800

    Update for lifecycle

--- UIKitDemoApp/Senior/SeniorViewController.swift
+++ UIKitDemoApp/Senior/SeniorViewController.swift
@@ -62,6 +62,15 @@ class SeniorViewController: BaseViewController {
         
     ]
     
+    init() {
+        print("init invoked")
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Senior-功能页"
